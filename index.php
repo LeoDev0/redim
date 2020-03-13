cd87ec42f72c248e073e533281921d293136721e
Leo <leo.80@protonmail.com>
2020-03-12 22:48:04 -0300

    Adicionando alguns icones, reorganizando a pasta 'images' e atualizando o gitignore

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="icon" type="image/ico" href="assets/images/favicon.ico">
+  <link rel="icon" type="image/ico" href="assets/images/icons/favicon.ico">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/styles.css">
   <title>RedIm: single page image resizer</title>
@@ -22,7 +22,15 @@
 
         <div class="mb-42 input-group shadow-sm">
           <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroupFileAddon01"><strong>Upload</strong></span>
+            <!-- <span class="input-group-text" id="inputGroupFileAddon01"><strong>Upload</strong></span> -->
+            <span class="input-group-text"
+                  id="inputGroupFileAddon01"
+                  data-toggle="tooltip" 
+                  data-html="true" 
+                  data-placement="bottom" 
+                  title="upload">
+                  <img src="assets/images/icons/upload.png">
+            </span>
           </div>
           <div class="custom-file">
             <input class="custom-file-input" id="browse" type="file" name="imagem" accept=".png, .jpg, .jpeg" aria-describedby="inputGroupFileAddon01">
@@ -40,7 +48,10 @@
                       data-html="true" 
                       data-placement="bottom" 
                       title="largura" 
-                      class="input-group-text">↔️</span>
+                      class="input-group-text">
+                      <!-- ↔️</span> -->
+                      <img src="assets/images/icons/arrow-width.png" alt="width-icon">
+                    </span>
               </div>
               <input class="form-control" type="number" name="width">
               <div class="input-group-append">
@@ -56,7 +67,10 @@
                       data-html="true"
                       data-placement="bottom"
                       title="altura"
-                      class="input-group-text">↕️</span>
+                      class="input-group-text">
+                      <!-- ↕️</span> -->
+                      <img src="assets/images/icons/arrow-height.png" alt="height-icon">
+                    </span>
               </div>
               <input class="form-control" type="number" name="height">
               <div class="input-group-append">
@@ -72,7 +86,10 @@
                       data-html="true"
                       data-placement="bottom"
                       title="porcentagem"
-                      class="input-group-text">%</span>
+                      class="input-group-text">
+                      <!-- %</span> -->
+                      <img src="assets/images/icons/percentage.png" alt="percentage-icon">
+                    </span>
               </div>
               <input id="percentage" class="form-control" type="number" min="1" max="999">
             </div>
@@ -130,7 +147,7 @@
             $imagedata = ob_get_clean();
             echo '<center>';
               echo '<img class="resized-image" src="data:image/jpeg;base64,'.base64_encode($imagedata).'"/><br>';
-              echo '<a class="btn-lg btn-warning download-btn" href="data:image/jpeg;base64,'.base64_encode($imagedata).'" download><img class="download-icon" src="assets/images/download.png">Download</a>';
+              echo '<a class="btn-lg btn-warning download-btn" href="data:image/jpeg;base64,'.base64_encode($imagedata).'" download><img class="download-icon" src="assets/images/icons/download.png">Download</a>';
             echo '</center>';
 
           } else {
@@ -146,7 +163,7 @@
             $imagedata = ob_get_clean();
             echo '<center>';
               echo '<img class="resized-image" src="data:image/png;base64,'.base64_encode($imagedata).'"/><br>';
-              echo '<a class="btn-lg btn-warning download-btn" href="data:image/png;base64,'.base64_encode($imagedata).'" download><img class="download-icon" src="assets/images/download.png">Download</a>';
+              echo '<a class="btn-lg btn-warning download-btn" href="data:image/png;base64,'.base64_encode($imagedata).'" download><img class="download-icon" src="assets/images/icons/download.png">Download</a>';
             echo '</center>';
           }   
         }
